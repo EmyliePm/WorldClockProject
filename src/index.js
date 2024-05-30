fa8b2a5f376cc11b8463a5173a14a72663fcadb8
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-30 16:07:13 +0100

    move update times of different cities into their own function for clarity

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-setInterval(function () {
-  //current time for user
+//current time for user
+function updateCurrentTime() {
   let currentTimeElement = document.querySelector("#currentTimeZone");
   let currentTimeZone = moment.tz.guess();
   let currentTime = moment().tz(currentTimeZone);
   currentTimeElement.innerHTML = `Current time in ${currentTimeZone} is ${currentTime.format(
     "h:mm:ss [<small>]A[</small>]"
   )}`;
+}
 
-  //London
+//London
+function updateLondonTime() {
   let londonElement = document.querySelector("#london");
   let londonDateElement = londonElement.querySelector(".date");
   let londonTimeElement = londonElement.querySelector(".time");
@@ -16,20 +18,28 @@ setInterval(function () {
   londonTimeElement.innerHTML = londonTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
+}
 
-  //Paris
+//Paris
+function updateParisTime() {
   let parisElement = document.querySelector("#paris");
   let parisDateElement = parisElement.querySelector(".date");
   let parisTimeElement = parisElement.querySelector(".time");
   let parisTime = moment().tz("Europe/Paris");
   parisDateElement.innerHTML = moment().format(" Do dddd MMMM YYYY");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
 
-  //Tokyo
+//Tokyo
+function updateTokyoTime() {
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoDateElement = tokyoElement.querySelector(".date");
   let tokyoTimeElement = tokyoElement.querySelector(".time");
   let tokyoTime = moment().tz("Asia/Tokyo");
   tokyoDateElement.innerHTML = moment().format(" Do dddd MMMM YYYY");
   tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
-}, 1000);
+}
+setInterval(updateCurrentTime, 1000);
+setInterval(updateLondonTime, 1000);
+setInterval(updateParisTime, 1000);
+setInterval(updateTokyoTime, 1000);
