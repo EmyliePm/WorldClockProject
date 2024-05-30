05842c025a401067df99e237ddfff7aa8ece9d14
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-05-30 15:06:02 +0100

    added moment.js library

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//London
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time");
+// londonDateElement.innerHTML = "hi";
+// londonTimeElement.innerHTML = "hi";
